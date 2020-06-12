74676e4116da60a81369ae36769346041753a3e6
pmarti99 <parkerjmartin883@gmail.com>
2020-06-11 19:37:02 -0600

    updating package json as well as models

---
+++ models/items.js
@@ -0,0 +1,40 @@
+module.exports = function(sequelize, DataTypes) {
+    var Items = sequelize.define("Post", {
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        price: {
+            type: DataTypes.NUMBER,
+            allowNull: false,
+        },
+        description: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            len: [1,250],
+        },
+    //     title: {
+    //     type: DataTypes.STRING,
+    //     allowNull: false,
+    //     validate: {
+    //       len: [1]
+    //     }
+    //   },
+    //   body: {
+    //     type: DataTypes.TEXT,
+    //     allowNull: false,
+    //     len: [1]
+    //   }
+    });
+  
+    Items.associate = function(models) {
+      Items.belongsTo(models.Seller, {
+        foreignKey: {
+          allowNull: false
+        }
+      });
+    };
+  
+    return Items;
+  };
+  
