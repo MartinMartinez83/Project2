1133f35ddf3791bb678e44b955be7a52aa1f248a
Erin Otterstrom <hapiotter@gmail.com>
2020-06-19 20:28:58 -0600

    daily update

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -7,8 +7,8 @@ const isAuthenticated = require("../config/middleware/isAuthenticated");
 module.exports = function(app) {
 //send them directly to the landing page
   app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "../public/landing.html"))
-  })
+    res.render("landing",{});
+  });
 
   app.get("/signup", (req, res) => {
     // If the user already has an account send them to the members page
