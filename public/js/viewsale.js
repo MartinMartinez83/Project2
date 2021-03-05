4c3940a0f40bcec2c0bae1ca397afe33878d1a4c
pmarti99 <parkerjmartin883@gmail.com>
2021-03-04 21:14:32 -0700

    commit

--- public/js/viewsale.js
+++ public/js/viewsale.js
@@ -1,6 +1,8 @@
 $(document).ready(() =>{
     //function for viewing all sales, this might just be a simple function to pull everything
-    let 
+    $.get("api/items").then(data =>{
+        $(".itmes").text(data.items)
+    })
 
 
 
